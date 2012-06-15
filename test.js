327df2163fb3494df9763e2a141a8a49d4ceaba0
acidhax <mail@matbee.com>
2012-06-15 01:27:55 -0400

    Removing old files.

--- test.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	var server = new Server();
-	server.Execute("Hello", ["one","two","three"], function (fail, one, two, three) {
-		console.log(one);
-	});
-	console.log("hey!");
-});
